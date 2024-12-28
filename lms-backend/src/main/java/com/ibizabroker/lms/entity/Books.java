04868d56a1f4ce83a8bb3908bb228154ccb04417
Anida Nezovic <anezovic1@etf.unsa.ba>
2024-12-28 21:01:25 +0100

    db changed

--- lms-backend/src/main/java/com/ibizabroker/lms/entity/Books.java
+++ lms-backend/src/main/java/com/ibizabroker/lms/entity/Books.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 public class Books {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Integer bookId;
     String bookName;
 
--- lms-backend/src/main/java/com/ibizabroker/lms/entity/Users.java
+++ lms-backend/src/main/java/com/ibizabroker/lms/entity/Users.java
@@ -11,7 +11,7 @@ import java.util.Set;
 public class Users {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer userId;
     private String username;
     private String name;
